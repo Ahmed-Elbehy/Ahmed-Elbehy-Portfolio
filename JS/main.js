850e2e7c629eec98512e57b7417750c3caf3303c
Ahmed Elbehy <ahmedelbehy361@gmail.com>
2026-05-02 22:51:07 +0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let lastScrollTop = 0;
+window.onscroll = () => {
+  const navebar = document.getElementById("navebar");
+  const scrollTop =
+    document.documentElement.scrollTop 
+
+  if (scrollTop < lastScrollTop) {
+    navebar.classList.remove("navbar-hidden");
+    navebar.classList.add("navbar-visible");
+  } else {
+    navebar.classList.remove("navbar-visible");
+    navebar.classList.add("navbar-hidden");
+  }
+
+  lastScrollTop = scrollTop;
+  console.log(scrollTop);
+};
